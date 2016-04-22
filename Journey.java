2b1ceab88e3f67b0c44cf69ad2f98c19d36beac9
BellaRostampour <bahareh_1178@yahoo.com>
2016-04-22 14:13:13 +0800

    Added files via upload

---
+++ Journey.java
@@ -0,0 +1,44 @@
+
+
+
+public class Journey {
+    
+    
+    double fuelEco , fuelCost, fuelConsume;
+     
+    Journey()
+    {
+        
+        fuelCost = 0;
+        fuelConsume = 0;
+        fuelEco = 0;
+                
+    }
+    
+    void setFuelCost(double fCost)
+    {
+        fuelCost = fCost;
+    }
+    double getFuelCost()
+    {
+        return fuelCost;
+    }
+    
+    void setFuelConsume(double fConsum)
+    {
+        fuelConsume = fConsum;
+    }
+    double getFuelConsume()
+    {
+        return fuelConsume;
+    }
+    
+    void setFuelEco(double fEco)
+    {
+        fuelEco = fEco;
+    }
+    double getFuelEco()
+    {
+        return fuelEco;
+    }
+}
